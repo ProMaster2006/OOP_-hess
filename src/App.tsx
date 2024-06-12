e9f775350df986827eaa9d65cc59f0cdceab4191
ProMaster2006 <bogdanptost@gmail.com>
2024-06-12 11:52:35 +0300

    add block future

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ function App() {
   return (
     <div className='container'>
      <BoardComponent board={board} setBoard={setBoard}/>
+     <div className='feateru_block'>Feature fich</div>
     </div>
   )
 }
